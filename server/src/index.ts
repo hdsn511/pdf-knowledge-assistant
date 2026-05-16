9a57c686d86b8c7849c9176efc4dd27ffe3bc57c
htreinen <htreinen@wisc.edu>
2026-05-15 23:11:09 -0500

    fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,21 +4,25 @@ import fs from "fs";
 import { errorHandler } from "./middleware/errorHandler.js";
 import pdfRouter from "./routes/pdfRoutes.js";
 import queryRouter from "./routes/queryRoutes.js";
-import { googleEmbeddings } from "./config/aiConfig.js";
 
-
-fs.mkdirSync('./uploads', { recursive: true });
+fs.mkdirSync("./uploads", { recursive: true });
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://https://pdf-knowledge-assistant-plum.vercel.app/",
+    ],
+  }),
+);
 app.use(express.json());
 
 app.use("/api/query", queryRouter);
 app.use("/api/pdf", pdfRouter);
 
 app.use(errorHandler);
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
